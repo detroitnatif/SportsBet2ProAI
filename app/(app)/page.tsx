00a3b3ad507bed1d581ecd40ae19ddd41198e207
detroitnatif <tytydizzle124@gmail.com>
2024-04-28 10:22:15 +0200

    displaying messages

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+'use client';
+
+import React, { useState } from 'react'
 
 function ChatPage() {
+  const [fetching, setFetching ] = useState(true);
+  const [messages, setMessages] = useState<ThreadMessage[]>([]);
+
   return (
-    <div>
-     <h1> ChatPage
-        </h1>
+    <div className='w-screen h-screen flex flex-col bg-black text-white'>
+      <div className='flex-grow overflow-y-hidden p-80 space-y-2'>
+      {fetching && <div className='text-center font-bold'> Fetching... </div> }
+      {messages.length === 0 && !fetching && <div className='text-center font-bold'> No Bets... </div>}
+      </div>
+    
+      
     </div>
   )
 }
