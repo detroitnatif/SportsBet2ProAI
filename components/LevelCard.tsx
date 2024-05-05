6a4d740a1b1637573e4c525358cb3fbfaf0aa435
detroitnatif <tytydizzle124@gmail.com>
2024-05-05 11:50:34 +0200

    making notification request

--- components/LevelCard.tsx
+++ components/LevelCard.tsx
@@ -4,12 +4,16 @@ import React from 'react'
 interface LevelCardProps {
     name: string;
     description: string;
-    onClick: () => void;
+    selected: boolean;
+    onSelect: () => void;
 }
 
-const LevelCard: React.FC<LevelCardProps> = ({ name, description, onClick }) => {
+const selectedStyle = "ring-2 ring-yellow-500 bg-yellow-500 bg-opacity-10";
+const unselectedStyle = "hover:bg-gray-100";
+
+const LevelCard: React.FC<LevelCardProps> = ({ name, description, selected, onSelect }) => {
     return (
-        <div className="bg-white p-4 m-4 shadow rounded-lg cursor-pointer hover:bg-gray-100" onClick={onClick}>
+        <div className={`flex flex-col p-4 border border-gray-200 rounded ${selected ? selectedStyle : unselectedStyle}`} onClick={onSelect}>
             <h4 className="font-medium text-lg text-gray-900">{name}</h4>
             <p className="text-gray-600">{description}</p>
         </div>
--- components/ProfileContainer.tsx
+++ components/ProfileContainer.tsx
@@ -9,6 +9,7 @@ interface ProfileContainerProps {
 import { useState } from 'react';
 import LevelCard from './LevelCard';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const levels = [
     {id: 'NOVICE',
@@ -24,42 +25,71 @@ const levels = [
     description: "Recieve a bet three times a day based on your favorite teams and players"}
 ]
 
+type Difficulties = "EASY" | "INTERMEDIATE" | "PRO"
 
 
 export default function ProfileContainer({challengePreferences} : ProfileContainerProps) {
+    const [saving, setSaving] = useState(false)
     const [sendNotifications, setSendNotifications] = useState(challengePreferences.sendNotifications)
+    const [difficuly, setDifficulty] = useState(challengePreferences.challengeId)
+
+    const changeNotifications = () => {
+        setSendNotifications((prev) => (!prev));
+    };
+
+    const changeLevel = (levelId: Difficulties) => {
+        setDifficulty(levelId)
+    }
+    
 
     
-    const handleLevel = async() => {
-        try{await axios.post<{
+    const handleSave = async() => {
+        setSaving(true)
+        try{
+        const response = await axios.post<{
         success: boolean;
-        error?: string;
-        data?: string;
+        data?: ChallengePreferences;
+        message?: string;
     
-    }>('/api/challengeUpdate',{id, notification, challengeId})}
+    }>('/api/challengeUpdate',
+    {id: challengePreferences.id,
+    challengeId: difficuly, 
+    sendNotifications, 
+})
+    console.log(response)
+    if (!response.data.data || !response.data.success){
+        toast.error(response.data.message ?? "something went wrong")
+        return;
+    }}
+
     
     catch(e){
+        console.error(e);
+        toast.error("Something went wrong. Please try again.");
         return;
+    }finally{
+        setSaving(false);
     }
 };
+
 
   return (
     <div className='flex flex-col'>
       <div className='flex flex-row justify-between items-center mb-4'>
         <h1 className='font-bold text-2xl'>Challenge Level</h1>
-        <Button>Save</Button>
+        <Button onClick={handleSave}>{saving ? "Saving" : "Save"}</Button>
       </div>
       <div className='flex flex-row items-center justify-between mb-4 p-4 shadow rounded-lg'>
         <div>
             <h3 className='font-medium text-lg text-gray-900'>Push Notification</h3>
             <p>Receive push notifications when new challenges are available</p>
         </div>
-        <Switch checked={sendNotifications}></Switch>
+        <Switch checked={sendNotifications} onCheckedChange={changeNotifications}></Switch>
       </div>
       <div className='flex flex-col p-5 mb-10'>
         {levels.map((level) => (
-        <LevelCard key={level.id} name={level.name} description={level.description} onClick={() => handleLevel(level.id)} />
-        ))};
+        <LevelCard key={level.id} name={level.name} description={level.description} selected={level.id === difficuly} onSelect={() => changeLevel(level.id as Difficulties)} />
+        ))}
       </div>
     </div>
   )
